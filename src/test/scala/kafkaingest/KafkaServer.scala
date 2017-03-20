7f9a40a09e411203ee14b7f81efc4952c330445e
Satendra Tiwari <sattiwari18@gmail.com>
2017-03-21 03:27:10 +0700

    start zk test server

--- src/test/scala/kafkaingest/KafkaServer.scala
+++ src/test/scala/kafkaingest/KafkaServer.scala
@@ -1,10 +1,41 @@
 package kafkaingest
 
+import java.net.ServerSocket
+
+import org.apache.curator.test.TestingServer
+import org.slf4j.LoggerFactory
+
 object KafkaServer {
+
+
+  private def choosePort() = choosePorts(1).head
+
+  private def choosePorts(count: Int): List[Int] = {
+    val sockets = for {
+      i <- 0 until count
+    } yield new ServerSocket(i)
+
+    val socketList = sockets.toList
+    val ports = socketList.map(_.getLocalPort)
+    socketList.map(_.close())
+    ports
+  }
 
 }
 
 class KafkaServer {
+
+  import KafkaServer._
+
+  val log = LoggerFactory.getLogger(getClass)
+
+  val zkPort = choosePort()
+  val zkConnect = "127.0.0.1:" + zkPort
+
+//  start a zookeeper server
+  val zkServer = new TestingServer(zkPort)
+
+  
 
   def startup() = ???
 
