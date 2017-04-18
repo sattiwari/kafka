fa706fa00c7635eadf7f790da30ecf99199c2af3
Satendra Tiwari <sattiwari18@gmail.com>
2017-04-18 16:57:54 +0700

    custom serializer

---
+++ client/src/main/java/kafka/Customer.java
@@ -0,0 +1,24 @@
+package kafka;
+
+/**
+ * Created by stiwari on 4/18/2017 AD.
+ */
+public class Customer {
+
+    private int customerID;
+    private String customerName;
+
+    public Customer(int ID, String name) {
+        this.customerID = ID;
+        this.customerName = name;
+    }
+
+    public int getID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return customerName;
+    }
+
+}
